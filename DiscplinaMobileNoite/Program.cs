7254574e01e79fbd237a30e601b95f32286d7d75
PedroIghor <pedro_pucci01@hotmail.com>
2025-05-07 17:56:06 -0300

    feat teste

--- DiscplinaMobileNoite/Program.cs
+++ DiscplinaMobileNoite/Program.cs
@@ -61,18 +61,18 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthorization();
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
+//using var scope = app.Services.CreateScope();
+//var services = scope.ServiceProvider;
 
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
-}
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred during migration!");
-}
+//try
+//{
+//    var context = services.GetRequiredService<DataContext>();
+//    context.Database.Migrate();
+//}
+//catch (Exception ex)
+//{
+//    var logger = services.GetRequiredService<ILogger<Program>>();
+//    logger.LogError(ex, "An error occurred during migration!");
+//}
 
 app.Run();
